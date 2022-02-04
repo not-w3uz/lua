0012ba01930c2a96a59476a79e5eae9bb527fa06
not-w3uz <96645181+not-w3uz@users.noreply.github.com>
2022-02-04 18:04:29 +0400

    Update sh.lua

--- sh.lua
+++ sh.lua
@@ -1,95 +1,13 @@
---[[                                                                                                                                                                                                             
-  _   _  ____ _______ ______                                                                                                                                                                                                  
- | \ | |/ __ \__   __|  ____|                                                                                                                                                                                                 
- |  \| | |  | | | |  | |__                                                                                                                                                                                                    
- | . ` | |  | | | |  |  __|                                                                                                                                                                                                   
- | |\  | |__| | | |  | |____                                                                                                                                                                                                  
- |_|_\_|\____/  |_|  |______|         _       _                            _____ _    _          _______          __     _____  _                                    _                                  _   _               _ 
- |__   __| |   (_)                   (_)     | |                          / ____| |  | |   /\   |  __ \ \        / /\   |  __ \( )                                  | |                                | | | |             | |
-    | |  | |__  _ ___   ___  ___ _ __ _ _ __ | |_   _   _ ___  ___  ___  | |    | |__| |  /  \  | |__) \ \  /\  / /  \  | |__) |/ ___   ___  ___ _ ____   _____ _ __| |__   ___  _ __    _ __ ___   ___| |_| |__   ___   __| |
-    | |  | '_ \| / __| / __|/ __| '__| | '_ \| __| | | | / __|/ _ \/ __| | |    |  __  | / /\ \ |  _  / \ \/  \/ / /\ \ |  _  /  / __| / __|/ _ \ '__\ \ / / _ \ '__| '_ \ / _ \| '_ \  | '_ ` _ \ / _ \ __| '_ \ / _ \ / _` |
-    | |  | | | | \__ \ \__ \ (__| |  | | |_) | |_  | |_| \__ \  __/\__ \ | |____| |  | |/ ____ \| | \ \  \  /\  / ____ \| | \ \  \__ \ \__ \  __/ |   \ V /  __/ |  | | | | (_) | |_) | | | | | | |  __/ |_| | | | (_) | (_| |
-    |_|  |_| |_|_|___/ |___/\___|_|  |_| .__/ \__|  \__,_|___/\___||___/  \_____|_|  |_/_/    \_\_|  \_\  \/  \/_/    \_\_|  \_\ |___/ |___/\___|_|    \_/ \___|_|  |_| |_|\___/| .__/  |_| |_| |_|\___|\__|_| |_|\___/ \__,_|
-  _____     _           _              | |        _ _  __ _          _   _ _      __             _                 _   _                     _      _                   _       | |                                           
- |_   _|   (_)         | |             |_|       | (_)/ _(_)        | | (_) |    / _|           | |               | | (_)                   (_)    (_)                 | |      |_|                                           
-   | |      _ _   _ ___| |_   _ __ ___   ___   __| |_| |_ _  ___  __| |  _| |_  | |_ ___  _ __  | |__  _   _ _ __ | |_ _ _ __   __ _  __   ___  ___ _  ___  _   _ ___  | |__   ___  ___  ___                                  
-   | |     | | | | / __| __| | '_ ` _ \ / _ \ / _` | |  _| |/ _ \/ _` | | | __| |  _/ _ \| '__| | '_ \| | | | '_ \| __| | '_ \ / _` | \ \ / / |/ __| |/ _ \| | | / __| | '_ \ / _ \/ _ \/ __|                                 
-  _| |_    | | |_| \__ \ |_  | | | | | | (_) | (_| | | | | |  __/ (_| | | | |_  | || (_) | |    | | | | |_| | | | | |_| | | | | (_| |  \ V /| | (__| | (_) | |_| \__ \ | |_) |  __/  __/\__ \                                 
- |_____|   | |\__,_|___/\__| |_| |_| |_|\___/ \__,_|_|_| |_|\___|\__,_| |_|\__| |_| \___/|_|    |_| |_|\__,_|_| |_|\__|_|_| |_|\__, |   \_/ |_|\___|_|\___/ \__,_|___/ |_.__/ \___|\___||___/                                 
-          _/ |                                                                                                                  __/ |                                                                                         
-  _____  |__/ _             _ _                       _   _           _     _ _                                                |___/_   _   _           _                                  _                                  
- |_   _|     | |           ( ) |                     | | | |         | |   (_| )                                                  / _| | | | |         | |                                | |                                 
-   | |     __| | ___  _ __ |/| |_   ___  __ _ _   _  | |_| |__   __ _| |_   _|/ _ __ ___     _____      ___ __   ___ _ __    ___ | |_  | |_| |__   __ _| |_   ___  ___ _ ____   _____ _ __| |__   ___  _ __  _ __   ___ _ __  
-   | |    / _` |/ _ \| '_ \  | __| / __|/ _` | | | | | __| '_ \ / _` | __| | | | '_ ` _ \   / _ \ \ /\ / / '_ \ / _ \ '__|  / _ \|  _| | __| '_ \ / _` | __| / __|/ _ \ '__\ \ / / _ \ '__| '_ \ / _ \| '_ \| '_ \ / _ \ '__| 
-  _| |_  | (_| | (_) | | | | | |_  \__ \ (_| | |_| | | |_| | | | (_| | |_  | | | | | | | | | (_) \ V  V /| | | |  __/ |    | (_) | |   | |_| | | | (_| | |_  \__ \  __/ |   \ V /  __/ |  | | | | (_) | |_) | |_) |  __/ |    
- |_____|  \__,_|\___/|_| |_|  \__| |___/\__,_|\__, |  \__|_| |_|\__,_|\__| |_| |_| |_| |_|  \___/ \_/\_/ |_| |_|\___|_|     \___/|_|    \__|_| |_|\__,_|\__| |___/\___|_|    \_/ \___|_|  |_| |_|\___/| .__/| .__/ \___|_|    
-        _   ___                                __/ |                                                                                                                                                  | |   | |               
-       | | |__ \                              |___/                                                                                                                                                   |_|   |_|               
-   ___ | | __ ) |                                                                                                                                                                                                             
-  / _ \| |/ // /                                                                                                                                                                                                              
- | (_) |   <|_|                                                                                                                                                                                                               
-  \___/|_|\_(_)                                                                                                                                                                                                               
-                                                                                                                                                                                                                                                                                                                                                                                                                                                
-]]
-
-local PlaceID = game.PlaceId
-local AllIDs = {}
-local foundAnything = ""
-local actualHour = os.date("!*t").hour
-local Deleted = false
-local File = pcall(function()
-    AllIDs = game:GetService('HttpService'):JSONDecode(readfile("NotSameServers.json"))
-end)
-if not File then
-    table.insert(AllIDs, actualHour)
-    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
-end
-function TPReturner()
-    local Site;
-    if foundAnything == "" then
-        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100'))
-    else
-        Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+function teleport()
+    local x = {}
+    for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+    	if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+    		x[#x + 1] = v.id
+    	end
     end
-    local ID = ""
-    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-        foundAnything = Site.nextPageCursor
-    end
-    local num = 0;
-    for i,v in pairs(Site.data) do
-        local Possible = true
-        ID = tostring(v.id)
-        if tonumber(v.maxPlayers) > tonumber(v.playing) then
-            for _,Existing in pairs(AllIDs) do
-                if num ~= 0 then
-                    if ID == tostring(Existing) then
-                        Possible = false
-                    end
-                end
-                num = num + 1
-            end
-            if Possible == true then
-                table.insert(AllIDs, ID)
-                wait()
-                pcall(function()
-                    wait()
-                    game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
-                end)
-                wait(4)
-            end
-        end
+    if #x > 0 then
+    	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
     end
 end
 
-function Teleport()
-    while wait() do
-        pcall(function()
-            TPReturner()
-            if foundAnything ~= "" then
-                TPReturner()
-            end
-        end)
-    end
-end
-
-return Teleport
+return teleport
